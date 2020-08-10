8ad84ad48b525d590211be5ffa4f413767bada0a
kyle cieskiewicz <kylecieskiewicz@kyles-MacBook-Pro.local>
2020-08-10 16:49:15 -0500

    Serperated Navbar from Header

---
+++ src/components/Header.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Header() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+        <div className="container">
+          <a className="navbar-brand js-scroll-trigger" href="#page-top" style={{ color: 'red' }}>Home</a>
+          <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            Menu
+        <i className="fa fa-bars"></i>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarResponsive">
+            <ul className="navbar-nav text-uppercase ml-auto">
+              <li className="nav-item">
+                <a className="nav-link js-scroll-trigger" href="#services" style={{ color: 'white' }}>Twister Tales and Safety</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link js-scroll-trigger" href="#portfolio" style={{ color: 'white' }}>Shelters</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link js-scroll-trigger" href="#about" style={{ color: 'white' }}>About</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link js-scroll-trigger" href="#contact" style={{ color: 'white' }}>Contact</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+  )
+}
+
+
+export default Header;
