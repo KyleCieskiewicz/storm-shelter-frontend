0fe43ea831bb49c58a2af3db201eaeb6fda3bc58
kyle cieskiewicz <kylecieskiewicz@kyles-MacBook-Pro.local>
2020-08-26 08:30:30 -0500

    cleaned up Shelter component

--- src/components/Shelter.js
+++ src/components/Shelter.js
@@ -1,16 +1,3 @@
-// import React from 'react'
-
-// function Shelter() {
-//   return (
-//     <div>
-//       <h1>About</h1>
-//       <p>This is the TodoList app v1.0.0. It is part of a React crash course</p>
-//     </div>
-//   )
-// }
-
-
-// export default Shelter;
 
 import React, { Component } from 'react';
 
